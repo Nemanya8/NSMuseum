2b557b79ebb82870f5006661a3969b5be7790d27
Nemanja Matic <nmatic.b290.21@one.pmf.uns.ac.rs>
2024-06-17 18:37:08 +0200

    Final commit

--- src/main/java/com/pris/project/prisprojectmonolith/controller/RouteController.java
+++ src/main/java/com/pris/project/prisprojectmonolith/controller/RouteController.java
@@ -19,54 +19,53 @@ import com.pris.project.prisprojectmonolith.service.RouteService;
 
 import jakarta.validation.Valid;
 
-
 @RestController
 @RequestMapping("/routes")
 public class RouteController {
-	
-	@Autowired
-    private RouteService routeService;
-	
-	@PostMapping("/newRoute")
-    public ResponseEntity<Route> createRoute(@RequestBody @Valid Route route) {
-       Route createdRoute = routeService.createRoute(route);
-        return ResponseEntity.ok(createdRoute);
-    }
-	
-	@GetMapping
-    public ResponseEntity<List<Route>> getAllRoutes() {
-        List<Route> routes = routeService.getAllRoutes();
-        return ResponseEntity.ok(routes);
-    }
-	
-	 @GetMapping("/{idRoute}")
-	    public ResponseEntity<Route> getRouteById(@PathVariable int idRoute) {
-	        Optional<Route> route = routeService.getRoutekById(idRoute);
-	        if (route.isPresent()) {
-	            return ResponseEntity.ok(route.get());
-	        } else {
-	            return ResponseEntity.notFound().build();
-	        }
-	    }
-	 
-	 @PutMapping("/{idRoute}")
-	    public ResponseEntity<Route> updateUser(@PathVariable int idRoute, @RequestBody @Valid Route route) {
-	        try {
-	            Route updatedRoute = routeService.updateRoute(idRoute, route);
-	            return ResponseEntity.ok(updatedRoute);
-	        } catch (RuntimeException e) {
-	            return ResponseEntity.notFound().build();
-	        }
-	    }
 
-	    @DeleteMapping("/{idRoute}")
-	    public ResponseEntity<Void> deleteRoute(@PathVariable int idRoute) {
-	        try {
-	            routeService.deleteRoute(idRoute);
-	            return ResponseEntity.noContent().build();
-	        } catch (RuntimeException e) {
-	            return ResponseEntity.notFound().build();
-	        }
-	    }
+	@Autowired
+	private RouteService routeService;
+
+	@PostMapping("/newRoute")
+	public ResponseEntity<Route> createRoute(@RequestBody @Valid Route route) {
+		Route createdRoute = routeService.createRoute(route);
+		return ResponseEntity.ok(createdRoute);
+	}
+
+	@GetMapping("/getAllRoutes")
+	public ResponseEntity<List<Route>> getAllRoutes() {
+		List<Route> routes = routeService.getAllRoutes();
+		return ResponseEntity.ok(routes);
+	}
+
+	@GetMapping("/{idRoute}")
+	public ResponseEntity<Route> getRouteById(@PathVariable int idRoute) {
+		Optional<Route> route = routeService.getRoutekById(idRoute);
+		if (route.isPresent()) {
+			return ResponseEntity.ok(route.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/{idRoute}")
+	public ResponseEntity<Route> updateUser(@PathVariable int idRoute, @RequestBody @Valid Route route) {
+		try {
+			Route updatedRoute = routeService.updateRoute(idRoute, route);
+			return ResponseEntity.ok(updatedRoute);
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{idRoute}")
+	public ResponseEntity<Void> deleteRoute(@PathVariable int idRoute) {
+		try {
+			routeService.deleteRoute(idRoute);
+			return ResponseEntity.noContent().build();
+		} catch (RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
 
 }
